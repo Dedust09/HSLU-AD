54cdcc9775f53c8f421c3fbe433cbea2a186f179
Unknown <lhuerzeler@gmail.com>
2018-03-04 18:23:13 +0100

    Excercises SW02

---
+++ src/main/java/ch/hslu/ad/sw02/Allocation.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hslu.ad.sw02;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Luke
+ */
+public final class Allocation implements Comparable<Allocation>{
+
+    private int size;
+    private int address;
+    private int addressNext;
+
+    public Allocation(final int address, final int size) {
+        this.address = address;
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getAddress() {
+        return address;
+    }
+
+    public  void setAddressNext (final int addrNext){
+        addressNext = addrNext;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Allocation)) {
+            return false;
+        }
+        final Allocation other = (Allocation) obj;
+        return (other.getAddress() == this.getAddress()) && (other.getSize() == this.getSize());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.address, this.size);
+    }
+
+    @Override
+    public String toString() {
+        return ("Allocation[Address:" + this.getAddress() + "; Size:" + this.getSize() + "]");
+    }
+
+    @Override
+    public int compareTo(Allocation o) {
+        return Integer.compare(this.getAddress(), o.getAddress());
+    }
+}
---
+++ src/main/java/ch/hslu/ad/sw02/AllocationList.java
@@ -0,0 +1,18 @@
+package ch.hslu.ad.sw02;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class AllocationList {
+
+    private int adressFirst;
+    private int listSize;
+    private Collection<Allocation> allocList = new LinkedList<>();
+
+
+    public AllocationList(){
+
+    }
+
+
+}
