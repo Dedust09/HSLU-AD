78a62a036e475aaf710830a9ca5d2733c6e7af9f
lhuerzeler@gmail.com <llJfoZFv8dNmN9MRzpWt>
2018-03-05 17:05:14 +0100

    bla

--- src/main/java/ch/hslu/ad/sw02/Allocation.java
+++ src/main/java/ch/hslu/ad/sw02/Allocation.java
@@ -35,14 +35,14 @@ public final class Allocation implements Comparable<Allocation>{
     }
 
     @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
+    public boolean equals(final Object objfreha) {
+        if (this == objfreha) {
             return true;
         }
-        if (!(obj instanceof Allocation)) {
+        if (!(objfreha instanceof Allocation)) {
             return false;
         }
-        final Allocation other = (Allocation) obj;
+        final Allocation other = (Allocation) objfreha;
         return (other.getAddress() == this.getAddress()) && (other.getSize() == this.getSize());
     }
 
