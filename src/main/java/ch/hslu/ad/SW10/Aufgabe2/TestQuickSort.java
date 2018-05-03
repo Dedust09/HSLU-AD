2547e74ac365102cdf1b7b62cee9186952d926a3
lhuerzeler@gmail.com <llJfoZFv8dNmN9MRzpWt>
2018-05-03 14:50:49 +0200

    worked on EX A2 Exercise 2

---
+++ src/main/java/ch/hslu/ad/SW10/Aufgabe2/TestQuickSort.java
@@ -0,0 +1,23 @@
+package ch.hslu.ad.SW10.Aufgabe2;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TestQuickSort {
+
+    private static final Logger LOG = LogManager.getLogger(TestQuickSort.class);
+
+    public TestQuickSort () {
+
+    }
+
+    public static void main(final String[] args) {
+
+        char[] a = {'l','k','a','s','j','d','u','i','v','h','v','w','e','f','h'};
+
+        LOG.info("Unsortiert: " + a);
+        QuickSort.quickSort(a,0,15);
+        LOG.info("Sortiert:   ");
+
+    }
+}
